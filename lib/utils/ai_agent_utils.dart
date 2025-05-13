47a042f86fb4615ca19a90a5b6b0d9b98a32201d
imdavyking <yungdynamic53@gmail.com>
2025-05-13 16:57:39 +0100

    chore: updated

--- lib/utils/ai_agent_utils.dart
+++ lib/utils/ai_agent_utils.dart
@@ -4,10 +4,6 @@ import 'package:dash_chat_2/dash_chat_2.dart';
 abstract class Constants {
   static ChatUser user = ChatUser(id: "1");
 
-  static ChatUser ai = ChatUser(
-    id: "2",
-    firstName: walletName,
-    // profileImage:
-    //     "https://storage.googleapis.com/cms-storage-bucket/780e0e64d323aad2cdd5.png",
-  );
+  static ChatUser ai =
+      ChatUser(id: "2", firstName: walletName, profileImage: "assets/logo.png");
 }
