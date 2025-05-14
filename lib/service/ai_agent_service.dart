fc7f2510506781dc55972df218d7cdcfc0107350
imdavyking <yungdynamic53@gmail.com>
2025-05-14 11:58:42 +0100

    chore: updated

--- lib/service/ai_agent_service.dart
+++ lib/service/ai_agent_service.dart
@@ -5,7 +5,6 @@ import "package:cryptowallet/extensions/to_real_json_langchain.dart";
 import "package:cryptowallet/main.dart";
 import "package:cryptowallet/screens/navigator_service.dart";
 import "package:cryptowallet/utils/app_config.dart";
-import "package:cryptowallet/utils/rpc_urls.dart";
 import "package:dash_chat_2/dash_chat_2.dart" as dash_chat;
 import "package:langchain/langchain.dart" as lang_chain;
 import "package:flutter/material.dart";
