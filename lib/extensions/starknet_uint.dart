c0fb4f3aa06e3f79a4ae6eb6a86976dd9b996e81
imdavyking <yungdynamic53@gmail.com>
2025-05-23 22:23:14 +0100

    chore: testing

--- lib/extensions/starknet_uint.dart
+++ lib/extensions/starknet_uint.dart
@@ -1,24 +1,51 @@
-// Yes please, for the implementation I think we should have a generic one
 import 'package:starknet/starknet.dart';
 
-extension Uint256ListToCalldata on List<Uint256> {
-  List<Felt> toCalldata() {
-    return [
-      Felt.fromInt(length),
-      for (final uint in this) ...[uint.low, uint.high],
-    ];
-  }
+// Interface for objects that can be serialized to calldata
+abstract class ToCalldata {
+  List<Felt> toCalldata();
 }
 
-extension ListUint256ToCalldata on List<List<Uint256>> {
+// Extension for lists of objects that implement ToCalldata
+extension ListToCalldata<T extends ToCalldata> on List<T> {
   List<Felt> toCalldata() {
     if (isEmpty) {
       return [Felt.zero];
     }
-    final convertedList = map((e) => e.toCalldata()).toList();
     return [
-      Felt.fromInt(length),
-      ...convertedList.expand((list) => list),
+      Felt.fromInt(length), // Length prefix
+      ...expand((item) => item.toCalldata()), // Flatten serialized items
+    ];
+  }
+}
+
+// Extension for Felt to support ToCalldata
+extension FeltToCalldata on Felt {
+  List<Felt> toCalldata() => [this];
+}
+
+// Extension for Uint256 to support ToCalldata
+extension Uint256ToCalldata on Uint256 {
+  List<Felt> toCalldata() => [low, high];
+}
+
+// Extension for List<Felt> to support ToCalldata
+extension FeltListToCalldata on List<Felt> {
+  List<Felt> toCalldata() => [
+        Felt.fromInt(length),
+        ...this,
+      ];
+}
+
+// Extension for nested lists of ToCalldata types
+extension NestedListToCalldata<T extends ToCalldata> on List<List<T>> {
+  List<Felt> toCalldata() {
+    if (isEmpty) {
+      return [Felt.zero];
+    }
+    return [
+      Felt.fromInt(length), // Length prefix for outer list
+      ...map((innerList) => innerList.toCalldata())
+          .expand((list) => list), // Flatten inner lists
     ];
   }
 }
