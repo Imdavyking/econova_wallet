1d7386fc56d0a3c4a4706708544e8e1118aa4f2f
imdavyking <yungdynamic53@gmail.com>
2025-05-23 23:42:52 +0100

    chore: updated

--- lib/extensions/starknet_uint.dart
+++ lib/extensions/starknet_uint.dart
@@ -1,5 +1,11 @@
 import 'package:starknet/starknet.dart';
 
+extension CallData on Uint256 {
+  List<Felt> toCalldata() {
+    return [low, high];
+  }
+}
+
 abstract class StarknetSerializable {
   List<Felt> toCalldata();
 }
@@ -7,11 +13,11 @@ abstract class StarknetSerializable {
 extension SerializableListToCalldata<T> on List<T> {
   List<Felt> toCalldata() {
     if (isEmpty) return [Felt.zero];
-    final dynamic first = this.first;
-    if (first is! StarknetSerializable) {
-      throw Exception(
-          'Element of type ${first.runtimeType} does not implement StarknetSerializable');
-    }
+    // final dynamic first = this.first;
+    // if (first is! StarknetSerializable) {
+    //   throw Exception(
+    //       'Element of type ${first.runtimeType} does not implement StarknetSerializable');
+    // }
 
     return [
       Felt.fromInt(length),
