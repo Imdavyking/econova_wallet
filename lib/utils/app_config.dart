199e56a73caeb2ba2cd6f437c03d20402252856c
imdavyking <yungdynamic53@gmail.com>
2025-07-23 01:45:51 +0100

    chore: updated

--- lib/utils/app_config.dart
+++ lib/utils/app_config.dart
@@ -139,7 +139,7 @@ const appUnlockTime = 'applockksksietimeal382';
 const blockExplorerPlaceholder = '{{TransactionHash}}';
 
 // enable
-bool enableTestNet = false;
+bool enableTestNet = true;
 const orange1 = Color.fromARGB(255, 233, 183, 9);
 
 // app theme
