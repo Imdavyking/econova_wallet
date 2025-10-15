00a567ba0f0491459c09514b8f42f239c4be6502
imdavyking <yungdynamic53@gmail.com>
2025-10-15 14:29:16 +0100

    chore: updated

--- lib/utils/format_money.dart
+++ lib/utils/format_money.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:intl/intl.dart' as intl;
 
 String formatMoney(num? money, [bool isTwoDPlace = false]) {
@@ -5,8 +6,17 @@ String formatMoney(num? money, [bool isTwoDPlace = false]) {
 
   double actualMoney = money.toDouble();
 
+  // Get current locale
+  final locale = WidgetsBinding.instance.platformDispatcher.locale;
+  final languageCode = locale.languageCode;
+  final countryCode = locale.countryCode ?? '';
+
+  // Use German locale for Germany
+  final localeString =
+      (languageCode == 'de' && countryCode == 'DE') ? 'de_DE' : 'en_US';
+
   if (actualMoney >= 1e6) {
-    return intl.NumberFormat.compact().format(actualMoney);
+    return intl.NumberFormat.compact(locale: localeString).format(actualMoney);
   }
 
   if (actualMoney.abs() < 0.00000001) {
@@ -14,16 +24,16 @@ String formatMoney(num? money, [bool isTwoDPlace = false]) {
   }
 
   if (isTwoDPlace && actualMoney.abs() > 0.001) {
-    return intl.NumberFormat.decimalPattern('en_US').format(actualMoney);
+    return intl.NumberFormat.decimalPattern(localeString).format(actualMoney);
   }
 
   if (actualMoney.abs() < 1 && actualMoney.abs() > 0.000001) {
-    return intl.NumberFormat('0.00000').format(actualMoney);
+    return intl.NumberFormat('0.00000', localeString).format(actualMoney);
   }
 
   if (actualMoney.abs() < 1 && actualMoney.abs() != 0) {
     return money.toStringAsFixed(8).replaceAll(RegExp(r"([.]*0)(?!.*\d)"), "");
   }
 
-  return intl.NumberFormat.decimalPattern().format(actualMoney);
+  return intl.NumberFormat.decimalPattern(localeString).format(actualMoney);
 }
