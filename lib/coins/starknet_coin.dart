44566b01c2a0294b41a854d720972b285974f3e9
imdavyking <yungdynamic53@gmail.com>
2025-12-04 21:20:11 +0100

    chore: updated

--- lib/coins/starknet_coin.dart
+++ lib/coins/starknet_coin.dart
@@ -1840,7 +1840,7 @@ List<StarknetCoin> getStarknetBlockchains() {
         default_: 'STRK',
         image: 'assets/starknet.png',
         api:
-            "https://starknet-mainnet.g.alchemy.com/starknet/version/rpc/v0_10/gpR0c9Le2dR45Fqit9OXTz6dtpf1HPfa",
+            "https://starknet-sepolia.g.alchemy.com/starknet/version/rpc/v0_10/gpR0c9Le2dR45Fqit9OXTz6dtpf1HPfa",
         geckoID: "starknet",
         payScheme: 'starknet',
         rampID: '',
