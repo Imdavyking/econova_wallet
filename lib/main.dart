c723cc449493e4da2cb13f8cc725b64f0d6f81af
imdavyking <yungdynamic53@gmail.com>
2026-04-08 23:31:28 +0100

    chore: updated

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@
 
 import 'dart:async';
 import 'dart:convert';
-
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
